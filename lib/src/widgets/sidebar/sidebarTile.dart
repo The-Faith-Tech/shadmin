2a52289293a9baf23fa121a91fe5c30ecf020f0b
Bijay Shrestha <bijayshrestha0908@gmail.com>
2024-09-06 21:55:54 +0545

    collapse options

--- lib/src/widgets/sidebar/sidebarTile.dart
+++ lib/src/widgets/sidebar/sidebarTile.dart
@@ -4,10 +4,29 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../shared/spacing.dart';
 import 'sidebarItem.dart';
 
-class SHSiderbarTile extends StatelessWidget {
+class SHSiderbarTile extends StatefulWidget {
   const SHSiderbarTile({super.key, required this.item, this.isFirst = false});
   final SHSiderBarItem item;
   final bool isFirst;
+
+  @override
+  State<SHSiderbarTile> createState() => _SHSiderbarTileState();
+}
+
+class _SHSiderbarTileState extends State<SHSiderbarTile> {
+  late bool collapsed;
+  @override
+  void initState() {
+    collapsed = !widget.isFirst;
+    super.initState();
+  }
+
+  void _toggle() {
+    setState(() {
+      collapsed = !collapsed;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,79 +34,125 @@ class SHSiderbarTile extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
-            children: [
-              if (item.icon != null && isFirst) ...[
-                Icon(
-                  item.icon,
-                  size: ks6.w,
-                ),
-                khsSmall,
+          InkWell(
+            onTap: widget.isFirst ? _toggle : null,
+            child: Row(
+              children: [
+                if (widget.item.icon != null && widget.isFirst) ...[
+                  Icon(
+                    widget.item.icon,
+                    size: ks6.w,
+                  ),
+                  khsSmall,
+                ],
+                widget.isFirst
+                    ? Expanded(
+                        child: Text(widget.item.title),
+                      )
+                    : Text(widget.item.title),
+                if (!widget.isFirst) ...[
+                  khsSmall,
+                  const Expanded(child: Divider()),
+                ],
+                if (widget.item.items.isNotEmpty && widget.isFirst) ...[
+                  khsTiny,
+                  Icon(
+                    collapsed
+                        ? Icons.keyboard_arrow_down
+                        : Icons.keyboard_arrow_up,
+                  )
+                ]
               ],
-              isFirst
-                  ? Expanded(
-                      child: Text(item.title),
-                    )
-                  : Text(item.title),
-              if (!isFirst) ...[
-                khsSmall,
-                const Expanded(child: Divider()),
-              ],
-              if (item.items.isNotEmpty && isFirst) ...[
-                khsTiny,
-                const Icon(Icons.keyboard_arrow_down)
-              ]
-            ],
+            ),
           ),
           kvsTiny,
-          ...item.items.map(
-            (e) => SHSiderbarMenuTile(
-              item: e,
-            ),
-          )
+          if (widget.isFirst) ...[
+            if (!collapsed)
+              ...widget.item.items.map(
+                (e) => SHSiderbarMenuTile(
+                  item: e,
+                ),
+              )
+          ] else ...[
+            ...widget.item.items.map(
+              (e) => SHSiderbarMenuTile(
+                item: e,
+              ),
+            )
+          ]
         ],
       ),
     );
   }
 }
 
-class SHSiderbarMenuTile extends StatelessWidget {
-  const SHSiderbarMenuTile({super.key, required this.item, this.level = 1});
+class SHSiderbarMenuTile extends StatefulWidget {
+  const SHSiderbarMenuTile(
+      {super.key, required this.item, this.level = 1, this.onTap});
   final SHSideBarMenuItem item;
   final int level;
+  final VoidCallback? onTap;
+
+  @override
+  State<SHSiderbarMenuTile> createState() => _SHSiderbarMenuTileState();
+}
+
+class _SHSiderbarMenuTileState extends State<SHSiderbarMenuTile> {
+  late bool collapsed;
+
+  @override
+  void initState() {
+    collapsed = true;
+    super.initState();
+  }
+
+  void _toggle() {
+    setState(() {
+      collapsed = !collapsed;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(left: level * ks2.w, top: ks1.h, bottom: ks1.h),
+      padding: EdgeInsets.only(
+          left: widget.level * ks2.w, top: ks1.h, bottom: ks1.h),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
-            children: [
-              if (item.icon != null) ...[
-                Icon(
-                  item.icon,
-                  size: ks6.w,
+          InkWell(
+            onTap: widget.item.children.isNotEmpty ? _toggle : widget.onTap,
+            child: Row(
+              children: [
+                if (widget.item.icon != null) ...[
+                  Icon(
+                    widget.item.icon,
+                    size: ks6.w,
+                  ),
+                ] else ...[
+                  SizedBox(width: ks6.w),
+                ],
+                khsSmall,
+                Expanded(
+                  child: Text(widget.item.title),
                 ),
-              ] else ...[
-                SizedBox(width: ks6.w),
+                if (widget.item.children.isNotEmpty) ...[
+                  khsTiny,
+                  Icon(collapsed
+                      ? Icons.keyboard_arrow_down
+                      : Icons.keyboard_arrow_up)
+                ]
               ],
-              khsSmall,
-              Expanded(
-                child: Text(item.title),
-              ),
-              if (item.children.isNotEmpty) ...[
-                khsTiny,
-                const Icon(Icons.keyboard_arrow_down)
-              ]
-            ],
-          ),
-          ...item.children.map(
-            (e) => SHSiderbarMenuTile(
-              item: e,
-              level: level + 1,
             ),
-          )
+          ),
+          kvsTiny,
+          if (!collapsed)
+            ...widget.item.children.map(
+              (e) => SHSiderbarMenuTile(
+                item: e,
+                level: widget.level + 1,
+              ),
+            )
         ],
       ),
     );
